a0ee645d546c5512801d374d9bf993f7cd42dea2
Dave Baskin <dfbaskin@gmail.com>
2023-01-25 18:19:38 -0500

    Adding build script.

---
+++ scripts/build-dist.ps1
@@ -0,0 +1,19 @@
+$rootPath = Join-Path $PSScriptRoot ".." -Resolve
+
+Push-Location -Path $rootPath
+
+Push-Location -Path repo-one
+npx nx build host-app
+if(!$?) { exit }
+npx nx build interop-app
+if(!$?) { exit }
+Pop-Location
+
+Push-Location -Path repo-two
+npm install
+if(!$?) { exit }
+npx nx build addon-app
+if(!$?) { exit }
+Pop-Location
+
+Pop-Location
